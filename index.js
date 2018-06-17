664c814cb6915e01b1eed25b20e3880a7aa4cde4
Michael Cline <mrc5163@arl.psu.edu>
2018-06-17 18:51:24 -0400

    Update with tagged information

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  res.end('The {"Location": "Philadelphia"} {"Organization": "Flyers"} are the best hockey team in the {"Organization": "NHL"}, and {"Person": "Claude Giroux"} should have won {"Organization": "MVP"}.\n');
 });
 
 server.listen(PORT, () => {
