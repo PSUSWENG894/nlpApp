b4be454ef0d95cc12b77afa6243717a1053016da
Michael Cline <mrc5163@arl.psu.edu>
2018-06-24 14:00:51 -0400

    Test user input into form

--- hello.js
+++ hello.js
@@ -1,6 +1,23 @@
 var http = require('http');
+var util = require('util');i
+var fs = require('fs');
+var url = require('url');
 
-http.createServer(function (req, res) {
+var server = http.createServer(function (req, res) {
+    var url_parts = url.parse(req.url, true);
+    util.log(url_parts);
+    if (url_parts.pathname == '/')
+	fs.readfile('./nodeforms/nlpform.html', function(error, data) {
+		res.end(data);
+	});
+    else if(url_parts == '/tagInput')
+	tagInput(res,url_parts);
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World!');
-}).listen(443);
+});
+
+server.listen(443);
+util.log('Server listening on port 443');
+
+function tagInput(res, url_parts) {
+	res,end("Data passed to tagInput -> input: " + url_parts.query.inputString);
+}
