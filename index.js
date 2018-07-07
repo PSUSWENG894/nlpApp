a0bced8b2720be056e5cfbab7cf683af4e9beda5
Michael Cline <mrc5163@arl.psu.edu>
2018-07-07 09:56:43 -0400

    Test form again

--- index.js
+++ index.js
@@ -1,53 +1,57 @@
-var http = require('http');
-var util = require('util');
-var fs = require('fs');
-var url = require('url');
-var nlp = require('compromise');
+var http = require("http");
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: true });
 
 var PORT = process.env.PORT || 5000;
 
-var server = http.createServer(function (req, res) {
-	if (req.method === 'POST' || req.methof === 'post') {
-	    var data = '';
-	
-	    req.on('data', function(chunk) {
-	      data += chunk;
-	    });
-	
-  	  req.on('end', function() {
-  	    // parse the data
-  	    //foo();
-		  tagInput(data);
-  	  });
-  	} else if (req.method === 'GET' || req.method === 'get') {
-		fs.readFile('./nodeForm/nlpForm.html', function(error, data) {
-			res.end(data);
-		});
-	}
-    var url_parts = url.parse(req.url, true);
-    console.log(url_parts);
-    res.writeHead(200, {'Content-Type': 'text/html'});
+// Running Server Details.
+var server = app.listen(PORT, function () {
+  var host = server.address().address
+  var port = server.address().port
+  console.log("Example app listening at %s:%s Port", host, port)
 });
-
-//const server = http.createServer((req, res) => {
-//  res.statusCode = 200;
-//  res.setHeader('Content-Type', 'text/plain');
-//  res.end('The {"Location": "Philadelphia"} {"Organization": "Flyers"} are the best hockey team in the {"Organization": "NHL"}, and {"Person": "Claude Giroux"} should have won {"Organization": "MVP"}.\n');
-//});
-
-server.listen(PORT, () => {
-  util.log(`Server running ${PORT}/`);
+ 
+ 
+app.get('/form', function (req, res) {
+  var html='';
+  html +="<body>";
+  html += "<form method='post' action='/tagged' name='tagForm'>";
+  html += "<label>Please fill in the text area below to have the NLP tag it.</label><br/>";
+  html += "<textarea name='inputString' id='inputString' rows='8' cols='80' placeholder='Enter text for NLP here'></textarea><br/>";
+  html += "<label>Select from the output formats below</label><br/>";
+  html += "<input type='radio' name='outputFormat' value='XML'> XML<br/>";
+  html += "<input type='radio' name='outputFormat' value='LIST' checked> List<br/>";
+  html += "<input type='radio' name='outputFormat' value='JSON> JSON<br/>";
+  html += "<input type='submit' name='tagButton' id='tagButton' value='Tag'>";
+  html += "<input type='reset' name='resetButton' id='resetButton' value='Reset'>";
+  html += "</form>";
+        document.getElementById("outputString").innerHtml(tags);
+  html += "</body>";
+  res.send(html);
 });
+ 
+app.post('/tagged', urlencodedParser, function (req, res){
+  var reply='';
+  var input = req.body.inputString;
+  console.log(input);
+  var outputFormat = req.body.outputFormat;
+  console.log(outputFormat);
+  var tags = tagInput(input);
+  reply += tags;
+  res.send(reply);
+ });
 
-function tagInput(data) {
-	var inputString = data;
-	var tags = "People: \n";
-	console.log("Data passed to tagInput -> input: " + inputString);
-	var doc = nlp(inputSring);
-	tags = tags + doc.people().data();
-	tags = tags + "\n Places: \n";
-	tags = tags + doc.places().data();
-	tags = tags + "\n Organizations: \n";
-	tags = tags + doc.organizations().data();
-	document.getElementById("outputString").innerHtml(tags);
-}
+ function tagInput(data) {
+        var inputString = data;
+        var tags = "People: \n";
+        console.log("Data passed to tagInput -> input: " + inputString);
+        var doc = nlp(inputSring);
+        tags = tags + doc.people().data();
+        tags = tags + "\n Places: \n";
+        tags = tags + doc.places().data();
+        tags = tags + "\n Organizations: \n";
+        tags = tags + doc.organizations().data();
+	return tags;
+ }
