66775d5ea227b0464bf807d091122c776123cdb5
Michael Cline <mrc5163@arl.psu.edu>
2018-07-19 20:42:55 -0400

    initial commit of some new categories on the front end.

--- index.js
+++ index.js
@@ -16,37 +16,43 @@ var server = app.listen(PORT, function () {
  
  
 app.get('/', function (req, res) {
-  var html=printHtmlForm('','LIST');
+  var html=printHtmlForm('', 'LIST', null);
   res.send(html);
 });
  
 app.post('/tagged', urlencodedParser, function (req, res){
   var input = req.body.inputString;
   var outputFormat = req.body.outputFormat;
-  var reply=printHtmlForm(input, outputFormat);
+  var selections = req.body.selections;
+  console.log(selections);
+  var reply=printHtmlForm(input, outputFormat, selections);
   reply += "<br/><b>The tagged input is shown below:</b><br/>";
-  var tags = tagInput(input, outputFormat);
+  var tags = tagInput(input, outputFormat, selections);
   reply += tags;
   res.send(reply);
  });
 
- function tagInput(data, format) {
+ function tagInput(data, format, selections) {
 	if (format === 'LIST') {
-		return tagList(data);
+		return tagList(data, selections);
 	} else if (format === 'JSON') {
-		return tagJson(data);
+		return tagJson(data, selections);
 	} else if (format === 'XML') {
-		return tagXml(data);
+		return tagXml(data, selections);
 	} else {
 		return "Error, unknown output format."
 	}
  }
 
-function tagList(data) {
+function tagList(data, selections) {
         var inputString = data;
         var tags = "<textarea rows='10' cols='80' readonly='true'>People: &#13;&#10;";
         var doc = nlp(inputString);
-        tags = tags + doc.people().out();
+	console.log(doc.dates());
+	console.log(doc.phoneNumbers());
+	console.log(doc.values());
+	console.log(doc.money());
+	tags = tags + doc.people().out();
         tags = tags + "&#13;&#10;&#13;&#10;Places: &#13;&#10;";
         tags = tags + doc.places().out();
         tags = tags + "&#13;&#10;&#13;&#10;Organizations: &#13;&#10;";
@@ -55,7 +61,7 @@ function tagList(data) {
 	return tags;
 }
 
-function tagJson(data) {
+function tagJson(data, selections) {
         var inputString = data;
 	var outputData = "<textarea rows='10' cols='80' readonly='true'>" + data;
         var doc = nlp(inputString);
@@ -78,7 +84,7 @@ function tagJson(data) {
 	return outputData;
 }
 
-function tagXml(data) {
+function tagXml(data, selections) {
         var inputString = data;
 	var outputData = "<textarea rows='10' cols='80' readonly='true'>" + data;
         var doc = nlp(inputString);
@@ -101,17 +107,18 @@ function tagXml(data) {
 	return outputData;
 }
 
- function printHtmlForm(data, format) {
+ function printHtmlForm(data, format, selections) {
   var html='';
   html +="<body>";
+  html += "<h1>Welcome to the PSU SWENG 894 Group 3 Natural Language Processing App</h1>";
   html += "<form method='post' action='/tagged' name='tagForm'>";
-  html += "<label>Please fill in the text area below to have the NLP tag it.</label><br/>";
+  html += "<b><label>Please fill in the text area below to have the NLP tag it.</label></b><br/>";
   html += "<textarea name='inputString' id='inputString' rows='10' cols='80' placeholder='Enter text for NLP here'>";
   if (data && data !== '' && data !== 'undefined') {
 	  html += data;
   }
   html += "</textarea><br/>";
-  html += "<label>Select from the output formats below</label><br/>";
+  html += "<label><b>Select from the output formats below</b></label><br/>";
   html += "<input type='radio' name='outputFormat' value='XML'";
   if (format && format ==='XML') {
 	  html += " checked";
@@ -127,6 +134,14 @@ function tagXml(data) {
 	  html += " checked";
   }
   html += "> JSON<br/>";
+  html += "<b><label>Select the checkboxes below for the categories you want tagged</label></b><br/>";
+  html += "<input type='checkbox' name='selections' value='Person'> Person<br/>";
+  html += "<input type='checkbox' name='selections' value='Person'> Place<br/>";
+  html += "<input type='checkbox' name='selections' value='Person'> Organization<br/>";
+  html += "<input type='checkbox' name='selections' value='Person'> Date<br/>";
+  html += "<input type='checkbox' name='selections' value='Person'> Money<br/>";
+  html += "<input type='checkbox' name='selections' value='Person'> Percent<br/>";
+  html += "<input type='checkbox' name='selections' value='Person'> Phone Number<br/>";
   html += "<input type='submit' name='tagButton' id='tagButton' value='Tag'><br/>";
   html += "<input type='reset' name='resetButton' id='resetButton' value='Reset'><br/>";
   html += "</form>";
